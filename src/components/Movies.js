227eb66688e1e5395d1b7ffa1f42e26322bec544
Angel Gtiez <angel.gutz.gtiez@gmail.com>
2022-01-28 17:20:13 +0100

    creating Movies component

---
+++ src/components/Movies.js
@@ -0,0 +1,17 @@
+import {Component, React} from 'react';
+
+class Movies extends Component{
+
+    render(){
+
+        return(
+            <div>Hello World!</div>
+        );
+    }
+
+
+
+}
+
+
+export default Movies;
