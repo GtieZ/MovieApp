79088577012a94aca32d1933148f2d0d031f8fd4
Angel Gtiez <angel.gutz.gtiez@gmail.com>
2022-01-28 20:00:08 +0100

    adding Home component

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './components/Home';
 import Movies from './components/Movies';
 import Not_Found from './components/Not_found';
 
@@ -11,15 +12,10 @@ class Router extends Component {
         return (
             <BrowserRouter>
                 <Routes>
+                    <Route path='/' element={<Home />} ></Route>
+                    <Route path='/movie' element={<Movies />} ></Route>
 
-                    <Route path='/' element={<Movies />} ></Route>
-
-                    <Route path='/error' element={<Not_Found />} ></Route>
-
-
-
-
-
+                    <Route path='*' element={<Not_Found />} ></Route>
                 </Routes>
             </BrowserRouter>
         );
