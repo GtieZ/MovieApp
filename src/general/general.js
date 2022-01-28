cb674f1da07eeb6b20d9b1923d8cc34ed23a58a6
Angel Gtiez <angel.gutz.gtiez@gmail.com>
2022-01-28 18:32:39 +0100

    updating general.js with search and imege url

--- src/general/general.js
+++ src/general/general.js
@@ -1,5 +1,7 @@
 const general = {
-    api_key: '84ee704467841409c9c9b05e0645bfbd'
+    api_key: '84ee704467841409c9c9b05e0645bfbd',
+    search_url: 'https://api.themoviedb.org/3/search/movie?api_key=84ee704467841409c9c9b05e0645bfbd&query=',
+    image_url: 'https://image.tmdb.org/t/p/w200'
 }
 
 export default general;
