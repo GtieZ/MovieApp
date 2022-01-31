12ca25cc0e982dd37719c1b88eff8123ba035795
Angel Gtiez <angel.gutz.gtiez@gmail.com>
2022-01-31 14:46:44 +0100

    adding simple animations with GSAP

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import general from '../general/general';
 
 import MovieComponent from './MovieComponent';
+import general from '../general/general';
 
 
 class Movies extends Component {
     apiKey = general.api_key;
     queryUrl = general.search_url;
 
-    
     state = {
         movies: [],
         query: this.props.query,
         searched: false,
-        firstQuery: true
+        firstQuery: true,
+        onChange: false
     };
 
     getMovies = (searchString) => {
@@ -23,7 +23,8 @@ class Movies extends Component {
             this.setState({
                 movies: response.data.results,
                 searched: true,
-                firstQuery: false
+                firstQuery: false,
+                onChange: false
             });
         });
     };
@@ -31,12 +32,15 @@ class Movies extends Component {
 
     componentDidUpdate(prevProps) {
         if (prevProps.query !== this.props.query) {
-            if(this.props.query !== ''){
+            this.setState({ onChange: true })
+
+            if (this.props.query !== '') {
                 this.getMovies(this.props.query);
             }
-        }
-      }
 
+
+        }
+    }
 
 
     render() {
@@ -49,19 +53,19 @@ class Movies extends Component {
                 )
             });
             return (
-                <div className="mt-4">
+                <div className="mt-4 queryResult">
                     {listMovie}
                 </div>
             )
-        } else if(!this.state.searched){
+        } else if (!this.state.searched) {
             return <div className="text-light mt-3">
-                    {this.state.firstQuery? '' : 'cargando...'}
-                </div>
+                {this.state.firstQuery ? '' : 'cargando...'}
+            </div>
         }
         return <h3 className="text-light mt-5">
-                    No se encontraron peliculas para esta búsqueda
-                </h3>
-        
+            No se encontraron peliculas para esta búsqueda
+        </h3>
+
     }
 
 }
--- src/components/MyListComponent.js
+++ src/components/MyListComponent.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { gsap } from 'gsap';
 
 import general from '../general/general';
 import noDisponiblePic from '../assets/images/no_disponible.jpg';
@@ -10,13 +11,23 @@ const MyListComponent = () => {
 
     const reviewList = useSelector(state => state.myList);
 
+    useEffect(() => {
+        if (reviewList.length > 0) {
+            gsap.from('.list-item', {
+                x: -20,
+                delay: 0,
+                rotation: "+=360",
+                scale: 0.1
+            });
+        }
+    });
+
+
     if (reviewList.length === 0) {
         return <h4 className="text-light mt-5">Todavía no has valorado ninguna película</h4>
     }
-
     return (
         <div className="text-light align-items-center mt-5 container list-container">
-
             {
                 reviewList.map((item, index) => {
                     return (
@@ -29,13 +40,12 @@ const MyListComponent = () => {
 
                                 <p className="list-review">
                                     Valoración: <strong>{item.reviewValue} &#9734;</strong>
-                                </p>  
+                                </p>
                             </div>
                         </div>
                     )
                 })
             }
-
         </div>
     );
 };
